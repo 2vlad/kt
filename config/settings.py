7990111d65b56e953619e457cdc295b6849a2bda
Yuriy Khamzyaev <yura@astroshock.ru>
2017-05-26 11:42:49 +0300

    added MEDIA_URL to settings.py

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,7 @@ LANGUAGES_TUPLE = (Lang.En, )
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
